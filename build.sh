42f3966feb0c9114f2e799f8cffad5b93a44ff99
archification <archification@gmail.com>
2025-10-09 22:45:12 -0400

    build script update

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+#cargo install cross --git https://github.com/cross-rs/cross
+
+# linux
 #RUSTFLAGS="-Zlocation-detail=none" cargo build --release
-RUSTFLAGS="-Zlocation-detail=none" cargo build --target=x86_64-unknown-linux-musl --release
-#RUSTFLAGS="-Zlocation-detail=none" cargo build --target x86_64-pc-windows-gnu --release --verbose
+RUSTFLAGS="-Zlocation-detail=none" cross build --target=x86_64-unknown-linux-musl --release
+# windows
+#RUSTFLAGS="-Zlocation-detail=none" cross build --target x86_64-pc-windows-gnu --release --verbose
 #upx --best --lzma target/release/archbot
 upx --best --lzma target/x86_64-unknown-linux-musl/release/archbot
+#strip target/x86_64-pc-windows-gnu/release/webify.exe
 #upx --best --lzma target/x86_64-pc-windows-gnu/release/archbot.exe
